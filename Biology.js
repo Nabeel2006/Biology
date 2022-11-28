47df6d3e1c472769c254d5e693d899ba4e3144df
Nabeel2006 <108058622+Nabeel2006@users.noreply.github.com>
2022-11-28 22:17:42 +0200

    Add files via upload

---
+++ Biology.js
@@ -0,0 +1,19 @@
+function video() {
+    if (document.getElementById('video').style.display === "none") {
+
+        document.getElementById("video").style.display = "block";
+    } else {
+        document.getElementById("video").style.display = "none";
+
+    }
+}
+
+function types() {
+    if (document.getElementById('type').style.display === "none") {
+
+        document.getElementById("type").style.display = "block";
+    } else {
+        document.getElementById("type").style.display = "none";
+
+    }
+}
